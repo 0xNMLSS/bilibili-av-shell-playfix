ed612f2785964dd17845daf13cc14f70f718654f
XXLEMON <ADMIN@XXLEMON.XX.KG>
2026-07-03 02:52:32 +0900

    Update description in version 0.6.8 to include a test video link for improved user guidance.

--- bilibili_av_shell_playfix.user.js
+++ bilibili_av_shell_playfix.user.js
@@ -2,7 +2,7 @@
 // @name         解除B站版权BV视频404播放限制
 // @namespace    https://github.com/0xNMLSS
 // @version      0.6.8
-// @description  仅 /video/BV* · 不解番剧。通过 pagelist 与 UGC playurl 替换 view 数据, 实现版权 BV 壳在 view404 时的网页播放;
+// @description  仅 /video/BV* · 不解番剧。通过 pagelist 与 UGC playurl 替换 view 数据, 实现版权 BV 壳在 view404 时的网页播放;测试视频:https://www.bilibili.com/video/BV1GJ411x7h7/
 // @author       0xNMLSS
 // @supportURL   https://github.com/0xNMLSS/bilibili-av-shell-playfix
 // @compatible   chrome
